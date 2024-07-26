e64a34df1d997e28aaaefef35d14d7037b3a437c
Luka Kavtaradze <lukakavtaradze2001@gmail.com>
2024-07-27 03:14:32 +0400

    Initial commit with project setup

---
+++ src/main/java/com/example/ecommerceplatformspringboot/EcommercePlatformSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerceplatformspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercePlatformSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommercePlatformSpringBootApplication.class, args);
+    }
+
+}
